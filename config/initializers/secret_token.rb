cfb68eb0c1d350cbd895fb1ae6768ab5441df1c5
Michael Demetrios <michael@appheaval.com>
2011-02-16 15:45:17 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Destroid::Application.config.secret_token = '3b891441a88a514de17132fbfa086cd63eb0449fc6b2794e5417dfd5a2515cebaa8cb07a6aa65e8d0808f9e74693fbfdd50f4c4aab60f2a4ef41a54b743e1274'
