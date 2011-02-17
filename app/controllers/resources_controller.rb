5cbdda788ce671df79443885d99398e1ab35c85d
Meltemi <mdreg+github@mac.com>
2011-02-16 19:56:24 -0800

    added different versions of resources_controller.rb

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,47 +1,83 @@
 class ResourcesController < ApplicationController
-  respond_to :html, :json
-  
   # GET /resources
+  # GET /resources.xml
   def index
-    respond_with(@resources = Resource.all)
+    @resources = Resource.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @resources }
+    end
   end
 
   # GET /resources/1
+  # GET /resources/1.xml
   def show
-    respond_with(@resource = Resource.find(params[:id]))
+    @resource = Resource.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @resource }
+    end
   end
-  
+
   # GET /resources/new
+  # GET /resources/new.xml
   def new
-    respond_with(@resource = Resource.new)
+    @resource = Resource.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @resource }
+    end
   end
 
   # GET /resources/1/edit
   def edit
-    respond_with(@resource = Resource.find(params[:id]))
+    @resource = Resource.find(params[:id])
   end
 
   # POST /resources
+  # POST /resources.xml
   def create
-     @resource = Resource.new(params[:resource])
-     flash[:success] = 'Resource was successfully created.' if @resource.save
-     # @resource.save
-     respond_with(@resource) #, :location => resources_url)
+    @resource = Resource.new(params[:resource])
+
+    respond_to do |format|
+      if @resource.save
+        format.html { redirect_to(@resource, :notice => 'Resource was successfully created.') }
+        format.xml  { render :xml => @resource, :status => :created, :location => @resource }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @resource.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
   # PUT /resources/1
+  # PUT /resources/1.xml
   def update
     @resource = Resource.find(params[:id])
-    flash[:notice] = 'Resource has been updated.' if @resource.update_attributes(params[:resource])
-    respond_with(@resource)
+
+    respond_to do |format|
+      if @resource.update_attributes(params[:resource])
+        format.html { redirect_to(@resource, :notice => 'Resource was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @resource.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
   # DELETE /resources/1
+  # DELETE /resources/1.xml
   def destroy
     @resource = Resource.find(params[:id])
-    flash[:notice] = 'Successfully deleted resource.' if @resource.destroy
-    logger.info "Resource Destroyed: #{@resource.destroyed?}"
-    logger.info "Persisted after delete: #{@resource.persisted?}"
-    respond_with(@resource) #, :location => resources_url)
+    @resource.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(resources_url) }
+      format.xml  { head :ok }
+    end
   end
 end
